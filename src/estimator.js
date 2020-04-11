80cf9b2446e7b773c827efb556899d11b8dd8b19
kod3180 <kod3180@gmail.com>
2020-04-11 14:41:11 +0000

    Challenge 1 tried

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,21 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const { reportedCases } = data;
+  const estimator = {
+    data, // the input data you got
+    impact: {}, // your best case estimation
+    severeImpact: {} // your severe case estimation
+  };
+
+  const { impact, severeImpact } = estimator;
+
+  // Challenge 1
+  impact.currentlyInfected = reportedCases * 10;
+  severeImpact.currentlyInfected = reportedCases * 50;
+
+  impact.infectionsByRequestedTime = Math.trunc(impact.currentlyInfected * 512);
+  severeImpact.infectionsByRequestedTime = Math.trunc(severeImpact.currentlyInfected * 512);
+
+  return estimator;
+};
 
 export default covid19ImpactEstimator;
